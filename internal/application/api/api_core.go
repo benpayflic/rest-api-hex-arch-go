9ec682f3a26259a3ece9751ebdcd19b5506c1e9d
Ben Goodenough <ben@octy.ai>
2022-07-22 10:10:26 +0100

    Implemented base project structure

---
+++ internal/application/api/api_core.go
@@ -0,0 +1,10 @@
+package api
+
+// Application implements the APIPort interface
+type Application struct {
+}
+
+// Return new instance of Application
+func NewApplication() *Application {
+	return &Application{}
+}
---
+++ internal/ports/primary.go
@@ -0,0 +1,8 @@
+package ports
+
+// Driven ports
+
+// APIPort is the technology neutral
+// port for driving adapters
+type APIPort interface {
+}
