a7b30dd99d9dfd5f2c8d964ae2b566465397c93c
Ben Goodenough <ben@octy.ai>
2022-08-05 12:51:39 +0100

    Added driving REST adapter functionality

--- internal/adapters/framework/primary/rest/dto/cats.go
+++ internal/adapters/framework/primary/rest/dto/cats.go
@@ -3,6 +3,18 @@ package dto
 import "github.com/benpayflic/rest-api-hex-arch-go/internal/application/domain/cats"
 
 type CreateCatDTO struct {
+	Message string `json:"message"`
+}
+
+type GetCatDTO struct {
 	Message string   `json:"message"`
 	Cat     cats.Cat `json:"cat"`
+}
+
+type UpdateCatDTO struct {
+	Message string `json:"message"`
+}
+
+type DeleteCatDTO struct {
+	Message string `json:"message"`
 }
